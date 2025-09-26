10f423c38485b1495de0be8e4b9487e384502a9c
superded62 <168063472+superded62@users.noreply.github.com>
2025-09-26 06:46:15 +0300

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,23 +1,44 @@
-(function(){
+(function () {
     var plugin = {};
-    
+
     plugin.id = 'my_plugin';
     plugin.version = '1.0.0';
     plugin.name = 'Мой плагин';
     plugin.description = 'Пример простого плагина для Lampa';
 
-    // Инициализация
-    plugin.init = function(){
+    plugin.run = function () {
         console.log('Мой плагин запущен');
-        
-        // пример: добавляем пункт в меню
+
+        // Добавляем пункт в главное меню
         Lampa.Listener.send('menu_main', {
             type: 'add',
             title: 'Мой раздел',
             url: 'plugin://my_plugin'
         });
+
+        // Реакция на открытие меню
+        Lampa.Listener.follow('app', function (e) {
+            if (e.type === 'open' && e.url === 'plugin://my_plugin') {
+                Lampa.Activity.push({
+                    url: 'plugin://my_plugin/page',
+                    component: 'full',
+                    type: 'card',
+                    title: 'Привет из моего плагина',
+                    data: {
+                        results: [
+                            {
+                                title: 'Тестовый фильм',
+                                release_year: 2024,
+                                poster: 'https://upload.wikimedia.org/wikipedia/ru/c/c1/The_Matrix_Poster.jpg'
+                            }
+                        ]
+                    }
+                });
+            }
+        });
     };
 
-    // Регистрируем плагин
     Lampa.Plugins.add(plugin);
 })();
+
+
